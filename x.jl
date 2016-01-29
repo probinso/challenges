876d35a239e012a2e30ce8026ca4eb0ea6f2ce88
probinso <pmoss.robinson@gmail.com>
2016-01-29 02:40:24 -0800

    fizzbuzz

---
+++ x.jl
@@ -0,0 +1,15 @@
+#!/usr/bin/env julia
+
+function solution{I<:Integer}(max::I=100)
+    spaces = ndigits(max)
+    function CracklePop(input::I)
+        c = mod(input, 3) == 0 ? "Crackle" : ""
+        p = mod(input, 5) == 0 ? "Pop"     : ""
+        " "^(spaces-ndigits(input))*"$(input) $(c)$(p)"
+    end
+    for i in 1:max
+        println(CracklePop(i))
+    end
+end
+
+solution(999)
