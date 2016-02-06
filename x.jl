de21c672dbc03acc65cab61f4722b3e73efd2edf
Philip Robinson <pmoss.robinson@gmail.com>
2016-02-05 23:51:21 -0800

    Update x.jl

--- x.jl
+++ x.jl
@@ -3,8 +3,8 @@
 function solution{I<:Integer}(max::I=100)
     spaces = ndigits(max)
     function CracklePop(input::I)
-        c = mod(input, 3) == 0 ? "Crackle" : ""
-        p = mod(input, 5) == 0 ? "Pop"     : ""
+        c = mod(input, 3) != 0 ? "" : "Crackle"
+        p = mod(input, 5) != 0 ? "" : "Pop"
         " "^(spaces-ndigits(input))*"$(input) $(c)$(p)"
     end
     for i in 1:max
