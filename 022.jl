b7223c21b565e428ee5a325970be9219297bd729
probinso <pmoss.robinson@gmail.com>
2016-02-10 17:01:27 -0800

    solution using generators

--- 022.jl
+++ 022.jl
@@ -1,5 +1,50 @@
 #!/usr/bin/env julia
 
+abstract BSTree{T}
+
+type Nil{T} <: BSTree{T}
+end
+
+type Node{T} <: BSTree{T}
+    value::T
+    left ::BSTree{T}
+    right::BSTree{T}
+end
+
+
+isempty(tree::Node) = false
+isempty(tree::Nil)  = true
+
+
+function insert!{T}(value::T, tree::Nil{T})
+    retval::BSTree = Node{T}(value, Nil{T}(), Nil{T}())
+end
+
+
+function insert!{T}(value::T, tree::Node{T})
+    if value < tree.value
+        tree.left  = insert!(value, tree.left)
+    elseif value > tree.value
+        tree.right = insert!(value, tree.right)
+    end
+    tree
+end
+
+
+inorder(tree::Nil) = nothing
+function inorder(tree::Node)
+    if !isa(tree.left, Nil)
+        inorder(tree.left)
+    end
+    produce(tree.value)
+
+    if !isa(tree.right, Nil)
+        inorder(tree.right)
+    end
+end
+
+
+
 function readfile(filename)
     #= generators are lower memory profile =#
     f = open(filename)
@@ -19,15 +64,19 @@ end
 
 function solution(filename::AbstractString="p022_names.txt")
 
+    tree::BSTree = Nil{Any}()
+    for name in @task readfile(filename)
+        tree = insert!(name, tree)
+    end
+
     function score(name::AbstractString)
         char_score(c) = c - 'A' + 1
         sum(map(char_score, collect(name)))
     end
 
-    names = collect(@task readfile(filename))
     acc = 0
-    for (index, name) in enumerate(sort(names))
-        acc += index*score(name)
+    for (i, name) in enumerate(@task inorder(tree))
+        acc += i*score(name)
     end
     acc
 end
