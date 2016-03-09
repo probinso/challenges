fb0bb84f56772ff83d2d11a1c37730b0d2a2597e
probinso <pmoss.robinson@gmail.com>
2016-03-09 11:33:13 -0800

    codeval

--- x.jl
+++ x.jl
@@ -1,5 +1,6 @@
 #!/usr/bin/env julia
 
+# Insight
 function solution{I<:Integer}(max::I=100)
     spaces = ndigits(max)
     function FizzBuzz{S<:AbstractString}(input, fizz::S="Fizz", buzz::S="Buzz")
@@ -12,4 +13,20 @@ function solution{I<:Integer}(max::I=100)
     end
 end
 
-solution(999)
+# Codeval
+function solution(a::Integer, b::Integer, max::Integer)
+
+    function FizzBuzz(input, fizz="F", buzz="B")
+        c = mod(input, a) != 0 ? "" : fizz
+        p = mod(input, b) != 0 ? "" : buzz
+        fizzbuzz = "$(c)$(p)"
+        (fizzbuzz != "" ? fizzbuzz : "$(input)")*" "
+    end
+    for i in 1:max
+        print(FizzBuzz(i, "F", "B"))
+    end
+end
+
+toInt = s -> parse(Int, s)
+solution(map(toInt, ARGS)...)
+println()
